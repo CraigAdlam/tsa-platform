3f64e82d36aaebac1a5af15aee40a3838d670d43
CraigAdlam <cadlam87@gmail.com>
2026-04-13 10:51:46 -0700

    Added initial_sidebar_state=expanded to show sidebar

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-st.set_page_config(page_title="Top Shelf Analytics", layout="wide")
+st.set_page_config(
+    page_title="Top Shelf Analytics",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
 
 st.markdown("### Data Hub")
 st.caption("Select a dataset from the sidebar.")
